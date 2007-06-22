bec6359f6beaac0a0a8b611b95fce4af2568bbeb
guillaume.boudreau <guillaume.boudreau@00a6bf44-7133-0410-8af3-77819ba6536f>
2007-06-22 00:26:49 +0000

    Fix for eAccelerator breaking try...catch blocks!

--- config.php
+++ config.php
@@ -6,5 +6,8 @@ $secret  = 'xyz';
 // This is the URL where the application files can be found.
 $callbackurl = 'http://gallery.danslereseau.com/fb/';
 
+// This is your application URL, as defined in Facebook.
+$appurl = 'http://apps.facebook.com/gallery_import/';
+
 $facebook_config['debug'] = FALSE;
 ?>
